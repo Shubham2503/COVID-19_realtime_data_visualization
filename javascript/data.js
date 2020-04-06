ab27aa36aa760ae98910c72e097ffe00cfc61067
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-04-06 12:49:34 +0530

    display sorted graph

--- javascript/data.js
+++ javascript/data.js
@@ -16,11 +16,12 @@ fetch("https://covid-193.p.rapidapi.com/statistics", {
  	if(a.cases.active > 2000 && a.country != "World" && a.country != "All")
  	{
 	 	av_label.push(a.country);
-	 	av_data.push(a.cases.active);
+	 	av_data.push(a.cases.total);
  	}
  })
 
- console.log(av_data.length);
+ sorrt(av_data,av_label);
+
  console.log(av_data);
  console.log(av_label);
  console.log(dat);
@@ -29,6 +30,28 @@ fetch("https://covid-193.p.rapidapi.com/statistics", {
 .catch(err => {
 	console.log(err);
 });
+
+async function sorrt(a,b)
+{
+	let q = a.length;
+	for(let i = 0;i<q;i++)
+	{
+		for(let j = 0;j<q;j++)
+		{
+			if(a[i] > a[j])
+			{
+				let t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+
+				let p = b[i];
+				b[i] = b[j];
+				b[j] = p;
+			}
+		}
+	}
+	return b;
+}
 
 async function chartIt(){
 document.getElementById("status").innerHTML = " ";
@@ -60,10 +83,50 @@ var myChart = new Chart(ctx, {
 				'rgba(72, 44, 44, 0.2)',
 				'rgba(62, 195, 232, 0.2)',
 				'rgba(89, 160, 39, 0.2)',
+				'rgba(217, 19, 55, 0.2)',
+				'rgba(86, 80, 146, 0.2)',
+				'rgba(58, 218, 84, 0.2)',
+				'rgba(162, 218, 22, 0.2)',
+				'rgba(143, 88, 249, 0.2)',
+				'rgba(133, 138, 213, 0.2)',
+				'rgba(188, 108, 65, 0.2)',
+				'rgba(166, 76, 147, 0.2)',
+				'rgba(171, 41, 123, 0.2)',
+				'rgba(12, 71, 6, 0.2)',
+				'rgba(27, 42, 122, 0.2)',
+				'rgba(155, 162, 169, 0.2)',
+				'rgba(252, 87, 91, 0.2)',
+				'rgba(45, 28, 104, 0.2)',
+				'rgba(35, 147, 18, 0.2)',
+				'rgba(86, 125, 209, 0.2)',
+				'rgba(201, 201, 250, 0.2)',
+				'rgba(72, 44, 44, 0.2)',
+				'rgba(62, 195, 232, 0.2)',
+				'rgba(89, 160, 39, 0.2)',
 				'rgba(217, 19, 55, 0.2)'
             ],
             borderColor: [
                 'rgba(86, 80, 146, 1)',
+				'rgba(58, 218, 84, 1)',
+				'rgba(162, 218, 22, 1)',
+				'rgba(143, 88, 249, 1)',
+				'rgba(133, 138, 213, 1)',
+				'rgba(188, 108, 65, 1)',
+				'rgba(166, 76, 147, 1)',
+				'rgba(171, 41, 123, 1)',
+				'rgba(12, 71, 6, 1)',
+				'rgba(27, 42, 122, 1)',
+				'rgba(155, 162, 169, 1)',
+				'rgba(252, 87, 91, 1)',
+				'rgba(45, 28, 104, 1)',
+				'rgba(35, 147, 18, 1)',
+				'rgba(86, 125, 209, 1)',
+				'rgba(201, 201, 250, 1)',
+				'rgba(72, 44, 44, 1)',
+				'rgba(62, 195, 232, 1)',
+				'rgba(89, 160, 39, 1)',
+				'rgba(217, 19, 55, 1)',
+				'rgba(86, 80, 146, 1)',
 				'rgba(58, 218, 84, 1)',
 				'rgba(162, 218, 22, 1)',
 				'rgba(143, 88, 249, 1)',
